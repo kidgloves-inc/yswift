dd7824e45d9f97ef1fabbcedbae8006808677ae8
Stephania Thomas <stgambar@gambaroff.com>
2026-08-26 16:10:49 -0700

    Release 0.3.0-kidgloves.1

--- Package.swift
+++ Package.swift
@@ -50,8 +50,8 @@ if ProcessInfo.processInfo.environment["YSWIFT_LOCAL"] != nil {
 } else {
     FFIbinaryTarget = .binaryTarget(
             name: "yniffiFFI",
-            url: "https://github.com/y-crdt/yswift/releases/download/0.2.1/yniffiFFI.xcframework.zip",
-            checksum: "7377378b6d8bb628ff8e2847f73a6b7115705d53a822dd578fb9cbcf2bce1675"
+            url: "https://github.com/kidgloves-inc/yswift/releases/download/0.3.0-kidgloves.1/yniffiFFI.xcframework.zip",
+            checksum: "534244ec5e127421c7bf5f924a86294ef5aeb2ab69b5818791e53e1aaf2903ce"
     )
 }
 
